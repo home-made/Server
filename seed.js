b44d03872f93599721c7489a02ed00aebcb73dd9
jaimemendozadev <jaimemendozadev@gmail.com>
2017-05-19 20:55:53 -0700

    Continue work on seed data
    Having trouble seeding db with new seed data.

--- seed.js
+++ seed.js
@@ -1,14 +1,10 @@
 var UserData = require('./seeds/User.json');
-var ActiveData = require('./seeds/ActiveDish.json');
-var InactiveData = require('./seeds/InactiveDish.json');
-// var OrderData = require('./seeds/Order.json');
-// var ReviewData = require('./seeds/Review.json');
+var DishData = require('./seeds/Dish.json');
+var OrderData = require('./seeds/Order.json');
 
 var User = require('./server/db/Schema.js').User;
-var ActiveDish = require('./server/db/Schema.js').ActiveDish;
+var Dish = require('./server/db/Schema.js').Dish;
 var Order = require('./server/db/Schema.js').Order;
-var InactiveDish = require('./server/db/Schema.js').InactiveDish;
-var Review = require('./server/db/Schema.js').Review;
 require('dotenv').load();
 
 var mongoose = require('mongoose');
@@ -16,221 +12,96 @@ var mongoDB = process.env.DATABASE_URL;
 
 var dbCounter = 0;
 
-var startSeed = () => {
-   //drop user collection
-  console.log("Starting to drop old Users")
+
+
+
+var seedData = () => {
+
+
+  console.log("Starting to drop old Users");
   User.remove({}, function(err, deletedUsers){
     if(err) {
-      console.log('There was a problem deleting the users from the database. ', err);
+      console.log('There was a problem deleting the old users from the database. ', err);
     }
 
     dbCounter++;
     
-    //seed user collection
+    console.log("Start seeding new Users");
     User.create(UserData, function(error, newDB) {
       if (err) {
-        console.log('Seed data couldn\'t be saved to the Database. ', err);
+        console.log('User data couldn\'t be saved to the Database. ', err);
       }
       console.log('The user data was successfully seeded to the Database. ', newDB);
       
-      dbCounter === 18 ? closeDB() : dbCounter++; 
+      if (dbCounter === 3)
+        closeDB();
 
     });
   });
 
 
 
-  //drop active dish collection
-  ActiveDish.remove({}, function(err, deletedDishes){
+  console.log("Starting to drop old Dishes");
+  Dish.remove({}, function(err, deletedDishes){
     if(err) {
-      console.log('There was a problem deleting the active dishes from the database. ', err);
+      console.log('There was a problem deleting the old dishes from the database. ', err);
     }
+
     dbCounter++;
 
-    ActiveDish.create(ActiveData, function(error, newDB) {
+    Dish.create(DishData, function(error, newDB) {
       if (err) {
-        console.log('Seed data couldn\'t be saved to the Database. ', err);
+        console.log('Dish seed data couldn\'t be saved to the Database. ', err);
       }
-      console.log('The data was successfully seeded to the Database. ', newDB);
+      console.log('The dish data was successfully seeded to the Database. ', newDB);
 
-      dbCounter === 18 ? closeDB() : dbCounter++; 
+      if (dbCounter === 3)
+        closeDB();
 
     });
-  });
-
-
-
-  //Update ActiveDishes with User IDs
-  User.find({ firstName: 'Luke'}, function (err, user) {
-    if(err) {
-      console.log('There was a problem finding the user in the db. ', err);
-    }
-
-    dbCounter++;
-
-    var id = user._id;
-
-    ActiveDish.findOneAndUpdate({'name': 'Dumplings'}, { 'chefId': [id] }, function(err, updatedDish){
-      if(err) {
-        console.log('There was a problem finding the user in the db. ', err);
-      }
-      console.log('The updatedDish was ', updatedDish);
-
-      dbCounter === 18 ? closeDB() : dbCounter++; 
-    });
-
   });
   
 
-  User.find({ firstName: 'Leia'}, function (err, user) {
+  console.log("Starting to drop old Orders");
+  Order.remove({}, function(err, deletedOrders){
     if(err) {
-      console.log('There was a problem finding the user in the db. ', err);
-    }
-    dbCounter++;
-
-    var id = user._id;
-
-    ActiveDish.findOneAndUpdate({'name': 'Samosas'}, { 'chefId': [id] }, function(err, updatedDish){
-      if(err) {
-        console.log('There was a problem finding the user in the db. ', err);
-      }
-      console.log('The updatedDish was ', updatedDish);
-
-      dbCounter === 18 ? closeDB() : dbCounter++; 
-    });
-  });
-
-
-  User.find({ firstName: 'Han'}, function (err, user) {
-    if(err) {
-      console.log('There was a problem finding the user in the db. ', err);
+      console.log('There was a problem deleting the old orders from the database. ', err);
     }
 
     dbCounter++;
 
-    var id = user._id;
-
-    ActiveDish.findOneAndUpdate({'name': 'Tacos'}, { 'chefId': [id] }, function(err, updatedDish){
-      if(err) {
-        console.log('There was a problem finding the user in the db. ', err);
-      }
-      console.log('The updatedDish was ', updatedDish);
-
-      dbCounter === 18 ? closeDB() : dbCounter++; 
-    });
-  });
-
-
-
-
-
-  
-  //drop inactive dish collection
-  InactiveDish.remove({}, function(err, deletedInactiveDishs){
-    if(err) {
-      console.log('There was a problem deleting the inactive dishes from the database. ', err);
-    }
-
-    dbCounter++;
-
-    InactiveDish.create(InactiveData, function(error, newDB) {
+    Order.create(OrderData, function(error, newDB) {
       if (err) {
-        console.log('Seed data couldn\'t be saved to the Database. ', err);
+        console.log('Order seed data couldn\'t be saved to the Database. ', err);
       }
-      console.log('The inactive dish data was successfully seeded to the Database. ', newDB);
+      console.log('The order data was successfully seeded to the Database. ', newDB);
 
-      dbCounter === 18 ? closeDB() : dbCounter++; 
-
+      if (dbCounter === 3)
+        closeDB();
     });
   });
 
-
-
-  //Update InactiveDishes
-  User.find({ firstName: 'Luke'}, function (err, user) {
-    if(err) {
-      console.log('There was a problem finding the user in the db. ', err);
-    }
-
-    dbCounter++;
-
-    var id = user._id;
-
-    InactiveDish.findOneAndUpdate({'name': 'Dumplings'}, { 'chefId': [id] }, function(err, updatedDish){
-      if(err) {
-        console.log('There was a problem finding the user in the db. ', err);
-      }
-      console.log('The updatedDish was ', updatedDish);
-
-      dbCounter === 18 ? closeDB() : dbCounter++; 
-    });
-
-  });
-  
-
-  User.find({ firstName: 'Leia'}, function (err, user) {
-    if(err) {
-      console.log('There was a problem finding the user in the db. ', err);
-    }
-
-    dbCounter++;
-
-    var id = user._id;
-
-    InactiveDish.findOneAndUpdate({'name': 'Samosas'}, { 'chefId': [id] }, function(err, updatedDish){
-      if(err) {
-        console.log('There was a problem finding the user in the db. ', err);
-      }
-      console.log('The updatedDish was ', updatedDish);
-
-      dbCounter === 18 ? closeDB() : dbCounter++; 
-
-    });
-  });
-
-
-  User.find({ firstName: 'Han'}, function (err, user) {
-    if(err) {
-      console.log('There was a problem finding the user in the db. ', err);
-    }
-    dbCounter++;
-
-    var id = user._id;
-
-    InactiveDish.findOneAndUpdate({'name': 'Tacos'}, { 'chefId': [id] }, function(err, updatedDish){
-      if(err) {
-        console.log('There was a problem finding the user in the db. ', err);
-      }
-      console.log('The updatedDish was ', updatedDish);
-
-      dbCounter === 18 ? closeDB() : dbCounter++; 
-    });
-  });
 }
+
 var seedDatabase = () => {
 
   mongoose.connect(mongoDB);
   var db = mongoose.connection;
 
+
   db.on('error', console.error.bind(console, 'MongoDB connection error:'));
   
   db.on('connected', function() {
     console.log('Successfully connected to DB!');
-    startSeed();
+  
   });
-
- 
 
   var closeDB = () => {
     db.close(function(){
       console.log("The connection to the database has been terminated.");
     });
-  };
+  }; 
 
-
-
- 
-};
-
+}
 
 seedDatabase();
