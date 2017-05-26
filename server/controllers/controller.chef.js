280cd8bcc3624da8f310fb715cad24cf7b2e36aa
jaimemendozadev <jaimemendozadev@gmail.com>
2017-05-26 09:24:21 -0700

    Create test route to render chefs on map

--- server/controllers/controller.chef.js
+++ server/controllers/controller.chef.js
@@ -36,6 +36,20 @@ exports.findChefs = (req, res) => {
     });
 };
 
+
+exports.findChefsTest = (req, res) => {
+  //for testing map
+  console.log(req.body);
+  User.find({})
+    .then(user => {
+      res.send(user);
+    })
+    .catch(err => {
+      res.send(err);
+    });    
+};
+
+
 exports.findChefsByStyle = (req, res) => {
   console.log(req.body);
   Dish.find({
--- server/routers/router.chef.js
+++ server/routers/router.chef.js
@@ -5,6 +5,9 @@ const chefController = require("../controllers/controller.chef");
 Router.get("/chef/:chefId", chefController.getChefDetails);
 Router.get("/chef/", chefController.findChefs);
 
+//test route for map component - remove in final version
+Router.get("/chefTest/", chefController.findChefsTest);
+
 //find chefs by style
 Router.get("/chef/style/:styleId", chefController.findChefsByStyle);
 
