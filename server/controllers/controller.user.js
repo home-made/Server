60ce4af5611f8a3624779479a432a4de59f90a60
Yubin Kim <joeekimm@gmail.com>
2017-06-02 17:48:29 -0700

    updated controller to also accept users signature with s3

--- server/controllers/controller.user.js
+++ server/controllers/controller.user.js
@@ -1,10 +1,49 @@
+require("dotenv").load();
 const { User, CustomerReview, ChefReview } = require("../db/Schema");
 var findOneOrCreate = require('mongoose-find-one-or-create');
+var s3 = require('s3');
 
 
 exports.updateUser = (req, res) => {
   console.log('Inside Update User');
   var updatedUser = req.body;
+
+  if (req.body.pathname) {
+    var client = s3.createClient({
+      maxAsyncS3: 20,
+      s3RetryCount: 3,
+      s3RetryDelay: 1000,
+      multipartUploadThreshold: 20971520,
+      multipartUploadSize: 15728640,
+      s3Options: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+      },
+    })
+    var params = {
+      localFile: req.body.pathname,
+      s3Params: {
+        Bucket: "homemadesignatures",
+        Key: req.params.authId + '.png',
+      },
+    };
+    
+    var url = 's3.amazonaws.com/homemadesignatures/' + params.s3Params.Key;
+    updatedUser.signatureURL = url;
+
+    var uploader = client.uploadFile(params);
+    uploader.on('error', function(err) {
+      console.error("unable to upload:", err.stack);
+    });
+    uploader.on('progress', function() {
+      console.log("progress", uploader.progressMd5Amount,
+                uploader.progressAmount, uploader.progressTotal);
+    });
+    uploader.on('end', function() {
+      console.log("done uploading");
+    });
+  }
+
   User.findOneAndUpdate(
     { authId: req.params.authId },
     updatedUser,
