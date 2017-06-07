d881cbacfbed62a9591946579d27a7fc8c6fc66a
jaimemendozadev <jaimemendozadev@gmail.com>
2017-06-07 04:49:55 -0700

    Add new User seed and new prop on User Schema

--- server/db/Schema.js
+++ server/db/Schema.js
@@ -42,7 +42,8 @@ var OrderSchema = new Schema({
   cart: {},
   status: Number,
   date: { type: Date, default: Date.now },
-  cashTotal: Number
+  cashTotal: Number,
+  orderInstructions: String
 });
 
 UserSchema.plugin(findOneOrCreate);
