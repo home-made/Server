6868c3725d515f25e102d46ebe088c53fdfc5588
David de Sousa <admin@anatta.local>
2017-06-08 21:33:21 -0700

    Controllers to get orders now supports users

--- server/routers/router.order.js
+++ server/routers/router.order.js
@@ -5,19 +5,19 @@ const orderController = require("../controllers/controller.order");
 ////////////////////////ORDERS//////////////////////
 
 //get pending orders
-Router.get("/orders/0/:id", orderController.getPendingOrders);
+Router.get("/orders/0/:type/:id/", orderController.getPendingOrders);
 // //get accepted orders
-Router.get("/orders/1/:id", orderController.getAcceptedOrders);
+Router.get("/orders/1/:type/:id/", orderController.getAcceptedOrders);
 // //get completed orders
-Router.get("/orders/2/:id", orderController.getCompletedOrders);
+Router.get("/orders/2/:type/:id/", orderController.getCompletedOrders);
 // //get cancelled orders
-Router.get("/orders/3/:id", orderController.getCancelledOrders);
+Router.get("/orders/3/:type/:id/", orderController.getCancelledOrders);
 
 //chef update order- accept,deny,mark as complete
 Router.put("/orders/", orderController.updateOrder);
 
 //get orders from customer with specific id
-Router.get("/orders/:id", orderController.getCustomerOrders);
+Router.get("/orders/:id/", orderController.getCustomerOrders);
 
 //post new order request
 Router.post("/orders/", orderController.postNewOrder);
